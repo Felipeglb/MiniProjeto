f3cdfda0f4ed6e3bfca1035508db79aec11e9249
RICARDO BEZERRA <you@example.com>
2023-02-20 21:40:17 -0300

    v1

---
+++ MiniProjeto/MiniProjeto/frmCategoria.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+
+namespace MiniProjeto
+
+{
+    using System.Data;
+    using System.Data.SqlClient;
+    public partial class Form1 : Form
+    {
+        string stringConexao = " data source = Localhost; initial Catalog=T13_MiniProjeto;User ID=sa; password=123456";
+        private void TestarConexao()
+        {
+            SqlConnection conn = new SqlConnection(stringConexao);
+
+            try
+            {
+                conn.Open();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+                Application.Exit();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmUsuario_Load(object sender, EventArgs e)
+        {
+            TestarConexao();
+        }
+
+        private void btoCadastrar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        }
+}
