076c14422a613b5f0a498b18ec994049752dc2c3
Felipe Gabriel Lira Bezerra <felipe.gabriel.lb.2018@gmail.com>
2023-02-27 15:28:42 -0300

    v9

---
+++ MiniProjeto/MiniProjeto/frmLogin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MiniProjeto
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btoOK = new System.Windows.Forms.Button();
+            this.btoSair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btoSair);
+            this.groupBox1.Controls.Add(this.btoOK);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSenha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtLogin);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(250, 60);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(193, 23);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(250, 121);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(193, 23);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Senha";
+            // 
+            // btoOK
+            // 
+            this.btoOK.Location = new System.Drawing.Point(250, 162);
+            this.btoOK.Name = "btoOK";
+            this.btoOK.Size = new System.Drawing.Size(75, 23);
+            this.btoOK.TabIndex = 2;
+            this.btoOK.Text = "&Ok";
+            this.btoOK.UseVisualStyleBackColor = true;
+            this.btoOK.Click += new System.EventHandler(this.btoOK_Click);
+            // 
+            // btoSair
+            // 
+            this.btoSair.Location = new System.Drawing.Point(368, 162);
+            this.btoSair.Name = "btoSair";
+            this.btoSair.Size = new System.Drawing.Size(75, 23);
+            this.btoSair.TabIndex = 3;
+            this.btoSair.Text = "&Sair";
+            this.btoSair.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox txtLogin;
+        private Label label1;
+        private Button btoSair;
+        private Button btoOK;
+        private Label label3;
+        private TextBox txtSenha;
+        private Label label2;
+    }
+}
---
+++ MiniProjeto/MiniProjeto/frmLogin.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniProjeto
+{
+    public partial class frmLogin : Form
+    {
+        public static string stringConexao = " data source = Localhost; initial Catalog=T13_MiniProjeto;User ID=sa; password=123456";
+        private void TestarConexao()
+        {
+            SqlConnection conn = new SqlConnection(stringConexao);
+
+            try
+            {
+                conn.Open();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+                Application.Exit();
+            }
+        }
+
+            public frmLogin()
+        {
+            InitializeComponent();
+            
+        }
+        public static string IDUsuario = "";
+        public static string NomeUsuario = "";
+        public static string LoginUsuario = "";
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            TestarConexao();
+        }
+
+        private void btoOK_Click(object sender, EventArgs e)
+        { Boolean Valida = false;
+            string sql = "select * from usuario where " +
+                "login_Usuario = '" + txtLogin.Text + "' and " +
+                "senha_Usuario = '" + txtSenha.Text + "'";
+
+            
+
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            conexao.Open();
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Valida = true;
+                    IDUsuario = reader[0].ToString();
+                    NomeUsuario = reader[1].ToString();
+                    LoginUsuario = reader[2].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválidos");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            if (Valida)
+            {
+                MDIParent1 frm = new MDIParent1();
+                frm.Show();
+                this.Hide();
+            }
+        }
+    }
+}
